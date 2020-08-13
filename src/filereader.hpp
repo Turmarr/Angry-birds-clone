7998f9d9c2de40f63317de57083c201a9e463a2a
Tuomas Rislakki <tuomas.rislakki@aalto.fi>
2020-08-13 19:59:04 +0300

    working file reader, documentation in .hpp

--- src/filereader.hpp
+++ src/filereader.hpp
@@ -5,6 +5,40 @@
 #include <sstream>
 #include <exception>
 
+/*
+The files to be read must be placed into the builds folder.
+
+Function:
+The information is read into specific structures to make it easier to keep track of all the
+information. If the program is unable to open the file it throws a file_exception to communicate with the
+main program.
+
+File structure:
+# starts a new block the blocks are in order:
+1 Pigs
+2 Birds
+3 Objects
+4 groundpoints (maybe not necessary)
+
+It doesn't matter what is after the # as it only watches for the first character of each line, but
+it has to be the first character. It is possible to leave blocks away, then the vectors will just be empty.
+If a block in the middle is empty the # has to be present to tick the counter to the correct block.
+
+On every line the information is separated by blank spaces.
+
+Pigs:
+"type of pig" "how manieth it is to be shot"
+
+Birds:
+"type of bird" "position on x-axis" "position on y-axis"
+
+Objects:
+"type of object" "position on x-axis" "position on y-axis"
+
+Groundpoint: (This will be utilized if box2d custom ground is implemented)
+"position on x-axis" "position on y-axis"
+*/
+
 class file_exception : public std::exception {
     	const char * what () const throw ()
     {
